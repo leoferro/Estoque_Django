20e7e0f851198d27684691795f8b3a6c7dddd35d
Leonardo <leo9266@gmail.com>
2023-03-27 22:23:58 -0300

    model Item

--- web_estoque/app_estoque/models.py
+++ web_estoque/app_estoque/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Itens(models.Model):
+    item_id   = models.AutoField(primary_key=True)
+    nome      = models.CharField(max_length=30)
+    volum_ml  = models.IntegerField()
+    categoria = models.CharField(max_length=30)
