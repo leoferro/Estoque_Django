3e0a5cd541bffe1b78a1101e11572157cd1e5145
Leonardo <leo9266@gmail.com>
2023-04-17 23:30:19 -0300

    Pagina dos relatorios com tabela

--- web_estoque/app_estoque/views.py
+++ web_estoque/app_estoque/views.py
@@ -14,4 +14,20 @@ def cadastro(request):
     return render(request, 'cadastro_teste.html')
 
 def date(request):
-    return render(request, 'date.html')
+    retorno = {
+    }
+    produtos = []
+    if request.POST:
+        #print(request.POST)
+
+        retorno['inicio'] = request.POST['inicio']
+        retorno['fim'] = request.POST['fim']
+        retorno['categoria'] = request.POST['categoria']
+        retorno['periodo'] = request.POST['periodo']
+        retorno['tipo'] = request.POST['vendas-estoque']
+        retorno['produtos'] = produtos
+
+        produtos.append({'produto':'coca cola 2L Retornável', 'data_ref':'21/12/2022', 'custo_unitario':4.99, "valor_venda":5.99, 'quantidade':20,'lucro':1.00})
+        produtos.append({'produto': 'coca cola zero 2L Retornável', 'data_ref': '21/12/2022', 'custo_unitario': 4.99, "valor_venda": 5.99, 'quantidade': 10, 'lucro': 1.00})
+
+    return render(request, 'date.html', retorno)
