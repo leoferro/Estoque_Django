3fe32b4df094c2a3d6d17a824d08860d24d9da31
Leonardo <leo9266@gmail.com>
2023-04-19 23:34:47 -0300

    nome csv com data

--- web_estoque/app_estoque/views.py
+++ web_estoque/app_estoque/views.py
@@ -14,6 +14,7 @@ def teste(request):
 def cadastro(request):
     return render(request, 'cadastro_teste.html')
 
+
 def relatorio(request):
     retorno = {
     }
@@ -26,6 +27,10 @@ def relatorio(request):
         retorno['categoria'] = request.POST['categoria']
         retorno['periodo'] = request.POST['periodo']
         retorno['tipo'] = request.POST['vendas-estoque']
+
+        #Realizar a querry nos filtros escolhidos
+
+
         retorno['produtos'] = produtos
 
         produtos.append({'produto':'coca cola 2L Retornável', 'data_ref':'21/12/2022', 'custo_unitario':4.99, "valor_venda":5.99, 'quantidade':20,'lucro':1.00})
@@ -34,9 +39,9 @@ def relatorio(request):
     return render(request, 'relatorio.html', retorno)
 
 def download(request):
-    print(request.GET)
+    #print(request.GET)
     response = HttpResponse(content_type="text/csv")
-    response['Content-Disposition'] = 'attachment; filename="relatorio.csv"'
+    response['Content-Disposition'] = f'attachment; filename="relatorio_{request.GET["inicio"]}.csv"'
 
     #Fazer a Querry deste metodo para retornar os valores corretos
     campos = ['item', 'tipo', 'tamanho']
