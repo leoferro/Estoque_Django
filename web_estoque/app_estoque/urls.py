553d062d56652223abf51b2485059882e8627a60
Leonardo <leo9266@gmail.com>
2023-04-20 22:55:56 -0300

    add vendas e estoque table

--- web_estoque/app_estoque/urls.py
+++ web_estoque/app_estoque/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("", views.index, name="Estoque App"),
     path("teste", views.teste, name="Teste"),
     path("cadastro", views.cadastro, name="Cadastro"),
+
     path("relatorio", views.relatorio, name="Date"),
     path("download", views.download, name="download"),
 ]
--- web_estoque/app_estoque/views.py
+++ web_estoque/app_estoque/views.py
@@ -15,26 +15,65 @@ def cadastro(request):
     return render(request, 'cadastro_teste.html')
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def relatorio(request):
-    retorno = {
-    }
     produtos = []
+
+
+    retorno = {
+        'produtos':produtos
+    }
     if request.POST:
         #print(request.POST)
+        print(request.POST['inicio'])
 
-        retorno['inicio'] = request.POST['inicio']
-        retorno['fim'] = request.POST['fim']
-        retorno['categoria'] = request.POST['categoria']
-        retorno['periodo'] = request.POST['periodo']
-        retorno['tipo'] = request.POST['vendas-estoque']
+        if (request.POST['inicio']=='' or request.POST['fim']==''):
+            retorno['erro'] = "Preencha Todos os Campos!"
 
-        #Realizar a querry nos filtros escolhidos
+        else:
+            retorno['inicio']    = request.POST['inicio']
+            retorno['fim']       = request.POST['fim']
+            retorno['categoria'] = request.POST['categoria']
+            retorno['periodo']   = request.POST['periodo']
+            retorno['tipo']      = request.POST['vendas-estoque']
 
 
-        retorno['produtos'] = produtos
+        if request.POST['vendas-estoque'] == "Vendas":
+            #Realizar a querry nos filtros escolhidos
 
-        produtos.append({'produto':'coca cola 2L Retornável', 'data_ref':'21/12/2022', 'custo_unitario':4.99, "valor_venda":5.99, 'quantidade':20,'lucro':1.00})
-        produtos.append({'produto': 'coca cola zero 2L Retornável', 'data_ref': '21/12/2022', 'custo_unitario': 4.99, "valor_venda": 5.99, 'quantidade': 10, 'lucro': 1.00})
+            retorno['columns'] = ['Produto', 'Data Referência', 'Custo Unitario', 'Valor Venda', 'Quantidade', 'Lucro']
+
+
+            produtos.append({'produto':'Coca Cola Tradicional Garrafa 2L', 'data_ref':'21/12/2022', 'custo_unitario':4.99, "valor_venda":5.99, 'quantidade':20,'lucro':1.00})
+            produtos.append({'produto': 'Coca Cola Zero Garrafa 2L', 'data_ref': '21/12/2022', 'custo_unitario': 4.99, "valor_venda": 5.99, 'quantidade': 10, 'lucro': 1.00})
+
+
+        elif request.POST['vendas-estoque']=="Estoque":
+            #Realizar a querry nos filtros escolhidos
+
+            retorno['columns']   = ['Descricção do Produto','Data de Validade','Custo Unitario','Quantidade em estoque','Valor total Estoque']
+
+            produtos.append(
+                {'produto': 'Coca Cola Tradicional Garrafa 2L', 'data_validade': '21/12/2023', 'custo_unitario': 4.99,
+                  'quantidade': 20, 'valor_estoque': 99.80})
+            produtos.append({'produto': 'Coca Cola Zero Garrafa 2L', 'data_validade': '21/12/2023', 'custo_unitario': 4.99,
+                 'quantidade': 10, 'valor_estoque': 49.90})
 
     return render(request, 'relatorio.html', retorno)
 
@@ -44,10 +83,10 @@ def download(request):
     response['Content-Disposition'] = f'attachment; filename="relatorio_{request.GET["inicio"]}.csv"'
 
     #Fazer a Querry deste metodo para retornar os valores corretos
-    campos = ['item', 'tipo', 'tamanho']
+    campos = ['Item','Categoria', 'Data Ref', 'Preço Compra', "Preço Venda","Unidades", "Lucro"]
     valores = [
-        ['coca', 'garrafa', '2L'],
-        ['coca zero', 'garrafa', '2L']
+        ['Coca Cola Tradicional Garrafa 2L', 'Refrigerante','2023-04-01', 5.99,6.99,10,10.00 ],
+        ['Coca Cola Zero Garrafa 2L', 'Refrigerante', '2023-04-01', 5.99,6.99,6,6.00]
     ]
     writer = csv.writer(response, csv.excel)
     writer.writerow(campos)
