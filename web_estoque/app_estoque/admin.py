068f370febda45994a4545c90c64b0866ef9aee6
Leonardo <leo9266@gmail.com>
2023-04-30 18:16:14 -0300

    Inicio Banco de Dados

--- web_estoque/app_estoque/admin.py
+++ web_estoque/app_estoque/admin.py
@@ -7,3 +7,5 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.Itens)
+admin.site.register(models.Compra)
+admin.site.register(models.Item_Venda)
--- web_estoque/app_estoque/models.py
+++ web_estoque/app_estoque/models.py
@@ -3,8 +3,35 @@ from django.db import models
 # Create your models here.
 
 class Itens(models.Model):
-    item_id   = models.AutoField(primary_key=True)
-    nome      = models.CharField(max_length=30)
-    volum_ml  = models.IntegerField()
-    categoria = models.CharField(max_length=30)
+    item_id             = models.AutoField(primary_key=True)
+    nome                = models.CharField(max_length=30)
+    categoria           = models.CharField(max_length=30)
+    marca               = models.CharField(max_length=30, default="NA")
+    produto_sabor       = models.CharField(max_length=30, default="NA")
+    tipo                = models.CharField(max_length=30, default="NA")
+    volume              = models.CharField(max_length=30)
 
+    def __str__(self):
+        return f"{self.nome} - {self.marca} - {self.categoria} - {self.produto_sabor} - {self.tipo} {self.volume}"
+
+class Item_Venda(models.Model):
+    item_venda_id       = models.AutoField(primary_key=True)
+    fk_item_id          = models.ForeignKey('Itens', on_delete=models.PROTECT)
+    quantidade          = models.PositiveSmallIntegerField()
+    desconto            = models.FloatField()
+
+
+class Compra(models.Model):
+    compra_id           = models.AutoField(primary_key=True)
+    data_compra         = models.DateField()
+    numero_referencia   = models.PositiveBigIntegerField()
+    fornecedor          = models.CharField(max_length=30)
+    fk_item_id          = models.ForeignKey('Itens', on_delete=models.PROTECT)
+    quantidade          = models.PositiveSmallIntegerField()
+    custo_unitario      = models.FloatField()
+    validade            = models.DateField()
+    valor_de_venda      = models.FloatField()
+    restantes           = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return f"{self.fk_item_id.nome}"
