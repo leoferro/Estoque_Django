c6749cee471f750e7e2302f57a9faaea9ac6f3e7
Leonardo <leo9266@gmail.com>
2023-04-30 21:33:37 -0300

    metodos estaticos encontrar_em_estoque, encontrar_ultimo_valor, vendas_entre, add_venda

--- web_estoque/app_estoque/models.py
+++ web_estoque/app_estoque/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 
 # Create your models here.
@@ -14,11 +16,59 @@ class Itens(models.Model):
     def __str__(self):
         return f"{self.nome} - {self.marca} - {self.categoria} - {self.produto_sabor} - {self.tipo} {self.volume}"
 
+
 class Item_Venda(models.Model):
     item_venda_id       = models.AutoField(primary_key=True)
+    data_venda          = models.DateField(default=datetime.datetime.now)
     fk_item_id          = models.ForeignKey('Itens', on_delete=models.PROTECT)
     quantidade          = models.PositiveSmallIntegerField()
-    desconto            = models.FloatField()
+    desconto            = models.FloatField(default=0)
+
+    def __str__(self):
+        return f"{self.data_venda} - {self.quantidade} {self.fk_item_id.nome}"
+
+    @staticmethod
+    def vendas_entre(data_menor, data_maior):
+        '''
+        call -> Item_Venda.vendas_entre(data_menor, data_maior)
+
+        :param data_menor: Menor data da consulta
+        :param data_maior: Maior data da consulta
+        :return: Todas as vendas entre essas datas
+        '''
+        return Item_Venda.objects.filter(data_venda__lte = data_maior, data_venda__gte = data_menor)
+
+    @staticmethod
+    def add_venda(id_item, quantidade, desconto=0, data=''):
+        '''
+        Preferivel utilizar esse metodo para adicionar venda
+            pois ja modifica o estoque automaticamente
+
+        :param id_item: numero do id do item
+        :param quantidade: quantodade de itens
+        :param desconto: valor a ser descontado
+        :param data: qual o dia da venda
+        :return: None
+        '''
+        if data=="":
+            data = datetime.datetime.now()
+        item = Itens.objects.get(item_id=id_item)
+
+        while quantidade>0:
+            print(quantidade)
+            compra = Compra.encontrar_em_estoque(id_item).first()
+            print(compra.restantes)
+            venda = Item_Venda(data_venda=data, quantidade=quantidade, desconto=desconto, fk_item_id=item)
+            if compra.restantes >= quantidade:
+                compra.restantes -= quantidade
+                quantidade = 0
+            else:
+                venda.quantidade = compra.restantes
+                quantidade = quantidade - compra.restantes
+                compra.restantes = 0
+            venda.save()
+            compra.save()
+
 
 
 class Compra(models.Model):
@@ -34,4 +84,33 @@ class Compra(models.Model):
     restantes           = models.PositiveSmallIntegerField()
 
     def __str__(self):
-        return f"{self.fk_item_id.nome}"
+        return f"{self.data_compra} - {self.numero_referencia} - {self.quantidade} {self.fk_item_id.nome} resta - {self.restantes}"
+
+    @staticmethod
+    def encontrar_em_estoque(id):
+        '''
+        call -> Compras.encontrar_em_estoque(id)
+
+        :param id: Id do produto
+        :return: QuerrySet das compras do produto
+                    selecionado que ainda estão
+                    no estoque ordenados do
+                    mais antigo para o mais novo
+        '''
+        compras = Compra.objects.filter(fk_item_id_id = id)
+        compras = compras.filter(restantes__gt = 0)
+        compras = compras.order_by("data_compra")
+        return compras
+
+    @staticmethod
+    def encontrar_ultimo_valor(id):
+        '''
+        call -> Compras.encontrar_ultimo_valor(id
+
+        :param id: Id do produto
+        :return: Float com o valor mais da compra mais
+                    recente do produto
+        '''
+        compras = Compra.objects.filter(fk_item_id_id=id)
+        compras = compras.order_by("data_compra").last()
+        return compras.valor_de_venda
